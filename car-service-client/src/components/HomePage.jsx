cff14fcb61d753b0118f6f80ac5f01441973c5c8
Ramzan <ramzan4ik95@mail.ru>
2022-02-01 16:26:29 +0300

    добавил папку клиента

---
+++ car-service-client/src/components/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Main from './Main/Main'
+import Footer from './Footer/Footer'
+import Header from './Header/Header'
+
+const HomePage = () => {
+  return (
+    <>
+    <Header/>
+    <Main />
+    <Footer />
+    </>
+  );
+};
+
+export default HomePage;
